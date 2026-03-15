1766a09a38c83a92ed6f8928ca5c7ecb3bf8f09e
Shobhit Gaidhane <155177835+shobhit0663@users.noreply.github.com>
2026-03-15 10:33:46 +0530

    Refactor NumberPyramid to use constant for rows

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -1,22 +1,19 @@
 public class NumberPyramid {
-    private static final int MAX_ROWS = 5;
-
     public static void main(String[] args) {
-        int n = MAX_ROWS;
-
+        int n = 5;
         for (int i = 1; i <= n; i++) {
+            // Spacing for alignment
             for (int j = 1; j <= (n - i); j++) {
                 System.out.print("  ");
             }
-
+            // First half: 1 to i
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
-
+            // Second half: i-1 to 1
             for (int j = i - 1; j >= 1; j--) {
                 System.out.print(j + " ");
             }
-
             System.out.println();
         }
     }
